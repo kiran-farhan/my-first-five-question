36f352c59b5a84b2b38e76fe22ec8d69f294b5bb
kiran-farhan <159946236+kiran-farhan@users.noreply.github.com>
2024-03-14 00:35:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// question num: 2
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var personname = "Hello Eric,";
+var message = " would you like to learn some Python today?”";
+console.log(personname + message);
+// question num:3
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var person_name = "KiranFarhan";
+console.log(person_name.toLowerCase());
+console.log(person_name.toUpperCase());
+console.log("Titlecase:", person_name.toLocaleUpperCase());
+// question num:4
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+var quote = ' “A person who never made a mistake never tried anything new.”';
+var author = "Albert Einstein once said,";
+console.log(author + quote);
+// question num:5
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famousperson = "quaid_e_Azam ,";
+var message2 = ' “A person who never made a mistake never tried anything new.” ';
+console.log(famousperson + message2);
---
+++ app.ts
@@ -0,0 +1,36 @@
+// question num: 2
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+ 
+ let personname: string= "Hello Eric,"
+ let message: string= " would you like to learn some Python today?”"
+ console.log(personname+message);
+
+// question num:3
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+  let person_name: string= "KiranFarhan";
+  console.log( person_name.toLowerCase())
+  console.log(person_name.toUpperCase())
+  console.log("Titlecase:", person_name.toLocaleUpperCase())
+
+// question num:4
+
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+
+let quote: string= ' “A person who never made a mistake never tried anything new.”'
+let author: string="Albert Einstein once said,";
+console.log(author+quote)
+ 
+// question num:5
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+
+ let famousperson: string= "quaid_e_Azam ,";
+  let message2: string=' “A person who never made a mistake never tried anything new.” ';
+ 
+  console.log(famousperson+message2)
+
+
+
+
